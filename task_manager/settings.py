3852da4dca56361238380f7cbc5223e3a8eb6ad4
RustemYeldessov <eldesov1997@gmail.com>
2025-09-10 22:54:27 +0500

    fix(templates): add layout.html and fix error 500

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "webserver,127.0.0.1,localhost,.onrender.com").split(",")
 

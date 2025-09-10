0286e416210a109e8595e2016ab66d0d2a039b25
RustemYeldessov <eldesov1997@gmail.com>
2025-09-10 21:29:13 +0500

    feat(urls): configure routing for main app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,18 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from operator import index
-
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('Привет от Хекслета!')
+from .views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', IndexView.as_view(), name="home"),
 ]

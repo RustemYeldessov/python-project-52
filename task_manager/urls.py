a816fa6542fa610866581f4e45bc38bf3a7eda3d
RustemYeldessov <eldesov1997@gmail.com>
2025-09-11 23:32:50 +0500

    refactor: delete statuses path from urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('users/create/', user_views.UserCreateView.as_view(), name="users_create"),
     path('users/<int:pk>/update/', user_views.UserUpdateView.as_view(), name="users_update"),
     path('users/<int:pk>/delete/', user_views.UserDeleteView.as_view(), name="users_delete"),
-    path('statuses/', include('task_manager.statuses.urls')),
 
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),

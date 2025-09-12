f3be207e5470b07d9222f46d82c867bdae2b0aeb
RustemYeldessov <eldesov1997@gmail.com>
2025-09-12 23:49:51 +0500

    add(statuses): add app statuses

---
+++ task_manager/statuses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.StatusListView.as_view(), name='statuses_index'),
+    path('create/', views.CreateView.as_view(), name='statuses_create'),
+    path('<int:pk>/update/', views.UpdateView.as_view(), name='statuses_update'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='statuses_delete'),
+]
---
+++ task_manager/statuses/views.py
@@ -0,0 +1,48 @@
+from django.contrib.messages.context_processors import messages
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Status
+from .forms import StatusForm
+
+
+class StatusListView(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = 'statusese/index.html'
+    context_object_name = 'statuses'
+
+
+class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'statuses/create.html'
+    success_url = reverse_lazy('statuses_index')
+    success_message = 'Статус успешно создан'
+
+
+class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'status/update.html'
+    success_url = 'statuses_index'
+    success_message = 'Статус успешно изменен'
+
+
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
+    model = Status
+    template_name = 'statuses/delete.html'
+    success_url = reverse_lazy('statuses_index')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            return super().post(request, *args, **kwargs)
+        except Exception:
+            messages.error(request, 'Невозможно удалить статус, так как он используется')
+            return redirect('statuses_index')
+
+

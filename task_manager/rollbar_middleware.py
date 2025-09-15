e93c550574cfdd6ed2ed5d9975d16d48c3a054cf
RustemYeldessov <eldesov1997@gmail.com>
2025-09-15 19:33:13 +0500

    feat(rollbar): add custom middleware with user and extra metadata

---
+++ task_manager/rollbar_middleware.py
@@ -0,0 +1,19 @@
+from rollbar.contrib.django.middleware import RollbarNotifierMiddleware
+
+class CustomRollbarNotifierMiddleware(RollbarNotifierMiddleware):
+    def get_extra_data(self, request, exc):
+        return {
+            "trace_id": "aabbccddeeff",  # можно, например, генерировать UUID
+            "feature_flags": ["feature_1", "feature_2"],
+        }
+
+    def get_payload_data(self, request, exc):
+        if not request.user.is_anonymous:
+            return {
+                "person": {
+                    "id": request.user.id,
+                    "username": request.user.username,
+                    "email": request.user.email,
+                }
+            }
+        return {}

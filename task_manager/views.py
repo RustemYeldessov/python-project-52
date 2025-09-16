af6ced8642ee327a9f1508e1079b9131f4d9c0e4
RustemYeldessov <eldesov1997@gmail.com>
2025-09-16 21:02:52 +0500

    fix(auth): show login success message on index page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
 
 
 class IndexView(View):
     def get(self, request):
+        if request.session.pop("just_logged_in", False):
+            messages.success(request, "Вы залогинены")
         return render(request, "index.html")
-

0dd239aeded6a9c1e37dc725b569ae8be0451e6d
RustemYeldessov <eldesov1997@gmail.com>
2025-09-16 21:17:14 +0500

    fix(task_manager): show login success message; correct routes and logout label

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,15 +6,15 @@ from task_manager.users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name="home"),
+    path('', IndexView.as_view(), name="index"),
 
     path('users/', user_views.UsersListView.as_view(), name="users_index"),
     path('users/create/', user_views.UserCreateView.as_view(), name="users_create"),
     path('users/<int:pk>/update/', user_views.UserUpdateView.as_view(), name="users_update"),
     path('users/<int:pk>/delete/', user_views.UserDeleteView.as_view(), name="users_delete"),
 
-    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', user_views.UserLoginView.as_view(), name="login"),
+    path('logout/', user_views.UserLogoutView.as_view(), name="logout"),
 
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),

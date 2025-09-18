95141062ebe039372f5eda5f46fdf18df68c6b0b
RustemYeldessov <eldesov1997@gmail.com>
2025-09-18 14:19:11 +0500

    fix(forms): ensure TaskForm selects are populated
    - Added __init__ to TaskForm to set queryset for status, executor, and labels
    - This fixes Playwright test failures due to empty dropdowns

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from .models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+
+User = get_user_model()
 
 class TaskForm(forms.ModelForm):
     class Meta:
@@ -12,3 +17,10 @@ class TaskForm(forms.ModelForm):
             'executor': forms.Select(attrs={'class': 'form-control'}),
             'labels': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # гарантируем, что селекты не пустые
+        self.fields['status'].queryset = Status.objects.all()
+        self.fields['executor'].queryset = User.objects.all()
+        self.fields['labels'].queryset = Label.objects.all()
---
+++ task_manager/tasks/status_forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from task_manager.statuses.models import Status  # <- именно эта модель
+
+class TaskStatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = ["name"]
+        labels = {
+            "name": _("Name"),
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+        }
